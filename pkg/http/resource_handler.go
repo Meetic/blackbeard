73e53c607044447e29e36091f0d6f0094d6a0aac
Sébastien Le Gall <s.legall@meetic-corp.com>
2018-01-31 16:20:28 +0100

    Feat : Add namespace status route
    This feature if for monitoring purpose. We expose 2 new API endpoint
    letting the client know :
    1 - What is the current status of a given namespace by calling the
    route /inventories/:namespace/status
    2 - What is the global status of namespaces managed by blackbeard by
    calling the route /inventories/status
    
    TODO :
    * Add swagger doc

--- pkg/http/resource_handler.go
+++ pkg/http/resource_handler.go
@@ -30,3 +30,58 @@ func (h *Handler) ListServices(c *gin.Context) {
 
 	c.JSON(http.StatusOK, services)
 }
+
+func (h *Handler) GetStatus(c *gin.Context) {
+
+	_, err := h.config.InventoryService().Get(c.Params.ByName("namespace"))
+
+	if err != nil {
+		if notFound, ok := err.(files.ErrorInventoryNotFound); ok {
+			c.JSON(http.StatusNotFound, gin.H{"error": notFound.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	status, err := h.kubernetes.ResourceService().GetNamespaceStatus(c.Params.ByName("namespace"))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, struct {
+		Status string `json:"status"`
+	}{
+		Status: status,
+	})
+}
+
+func (h *Handler) GetStatuses(c *gin.Context) {
+
+	invs, _ := h.config.InventoryService().List()
+
+	var status []struct {
+		Namespace string `json:"namespace"`
+		Status    string `json:"status"`
+	}
+
+	for _, i := range invs {
+		s, err := h.kubernetes.ResourceService().GetNamespaceStatus(i.Namespace)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		status = append(status, struct {
+			Namespace string `json:"namespace"`
+			Status    string `json:"status"`
+		}{
+			Namespace: i.Namespace,
+			Status:    s,
+		})
+	}
+
+	c.JSON(http.StatusOK, status)
+}
