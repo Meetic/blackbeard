323c10659de9a6878f2859641077538ac088107b
Sébastien Le Gall <s.legall@meetic-corp.com>
2018-07-22 16:00:38 +0200

    Add api v1 and v2 and front web

---
+++ example/my-app/api/v1/app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	version = 1
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello API version %d\n", version)
+}
+
+func main() {
+	log.Print("API running")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":50051", nil)
+}
