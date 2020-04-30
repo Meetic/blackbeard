dbe419c2ce6ed321aedd7a8d98ae769a42beab87
Paul Boiseau <paulboiseau@gmail.com>
2020-04-30 15:56:38 +0200

    fix(job): List job status get condition

--- pkg/mock/pod.go
+++ pkg/mock/pod.go
@@ -12,7 +12,7 @@ type PodRepository struct {
 
 // GetPods of all the pods in a given namespace.
 // This method returns a Pods slice containing the pod name and the pod status (pod status phase).
-func (m* PodRepository) List(n string) (resource.Pods, error) {
+func (m *PodRepository) List(n string) (resource.Pods, error) {
 	args := m.Called(n)
 	return args.Get(0).(resource.Pods), args.Error(1)
 }
